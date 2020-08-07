5a40f2345b113d60dfaf781f0855ee608137b4d5
gira1992 <47272309+gira1992@users.noreply.github.com>
2020-08-07 13:44:05 +0100

    Add files via upload
    First Commit

---
+++ js/scripts.js
@@ -0,0 +1,102 @@
+/*!
+    * Start Bootstrap - Agency v6.0.2 (https://startbootstrap.com/template-overviews/agency)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+    */
+    (function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top - 72,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#mainNav",
+        offset: 74,
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+})(jQuery); // End of use strict
+
+
+//scroll effect
+
+
+function scrollEffect(){
+    var aboutContent = document.querySelector('.aboutBody');
+    var aboutPosition = aboutContent.getBoundingClientRect().top;
+    var screenPosition = window.innerHeight/1.8;
+    
+    if(aboutPosition < screenPosition){
+        aboutContent.classList.add('aboutEffect');
+    }
+    
+}
+
+
+window.addEventListener('scroll', scrollEffect);
+
+
+//typing effect
+
+const text = ["<>Justin Gira</>"];
+let count = 0;
+let index = 0;
+let currentText='';
+let letter = '';
+
+(function type(){
+    
+    if (count === text.length){
+        count = 0;
+    }
+    
+    currentText = text[count];
+    letter = currentText.slice(0, ++index);
+    
+    document.querySelector('.typing').textContent = letter;
+    if(letter.length === currentText.length){
+        count++;
+        index = 16;
+    }
+    
+    setTimeout(type, 300);
+    
+}());
